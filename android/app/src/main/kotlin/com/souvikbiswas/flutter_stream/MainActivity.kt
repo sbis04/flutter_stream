2c52248593c201278d02283bac8a81471c88d0c2
Souvik Biswas <sbis1999@gmail.com>
2021-01-27 08:40:00 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/souvikbiswas/flutter_stream/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.souvikbiswas.flutter_stream
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Stream',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
