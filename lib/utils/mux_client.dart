08d70068d1d247709d54abaff16bb962c1659a7c
Souvik Biswas <sbis1999@gmail.com>
2021-01-28 17:47:08 +0530

    Client update

--- lib/utils/mux_client.dart
+++ lib/utils/mux_client.dart
@@ -3,10 +3,18 @@ import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:flutter_stream/model/video_data.dart';
 import 'package:flutter_stream/res/string.dart';
-import 'package:flutter_stream/secrets.dart';
+// import 'package:web_socket_channel/io.dart';
+// import 'package:web_socket_channel/status.dart' as status;
+
+import '../res/string.dart';
+import '../res/string.dart';
 
 class MUXClient {
   Dio _dio = Dio();
+
+  // String _cookie;
+  // var channel = IOWebSocketChannel.connect(Uri.parse(
+  //     'ws://@$muxBaseUrl/video/v1/assets/Nh3DrtRN4j01T1cwmMdArqgUkYqJxdg402Au4qwL00idi00'));
 
   initializeDio() {
     // authToken format: {MUX_TOKEN_ID}:{MUX_TOKEN_SECRET}
@@ -25,16 +33,51 @@ class MUXClient {
   }
 
   storeVideo() async {
+    Response response;
+
     try {
-      Response response = await _dio.post(
+      response = await _dio.post(
         "/video/v1/assets",
         data: {
           "input": demoVideoUrl,
           "playback_policy": playbackPolicy,
         },
       );
+    } catch (e) {
+      print('Error starting build: $e');
+    }
 
-      if (response.statusCode == 201) {
+    if (response.statusCode == 201) {
+      VideoData videoData = VideoData.fromJson(response.data);
+
+      String status = videoData.data.status;
+
+      print(status);
+
+      while (status == 'preparing') {
+        print('check');
+        await Future.delayed(Duration(seconds: 1));
+        videoData = await checkPostStatus(videoId: videoData.data.id);
+        status = videoData.data.status;
+      }
+
+      // print('Video READY, id: ${videoData.data.id}');
+
+      return videoData;
+    }
+
+    return null;
+  }
+
+  Future<VideoData> checkPostStatus({String videoId}) async {
+    try {
+      Response response = await _dio.get(
+        "/video/v1/assets/$videoId",
+      );
+
+      // print(response.data);
+
+      if (response.statusCode == 200) {
         VideoData videoData = VideoData.fromJson(response.data);
 
         return videoData;
@@ -45,4 +88,17 @@ class MUXClient {
 
     return null;
   }
+
+  // Future<Stream<dynamic>> _positionsStream(
+  //     {String serverUrl,
+  //     String email,
+  //     String password,
+  //     String protocol = "http"}) async {
+  //   if (_cookie == null) {
+  //     await _getConnection(email: _email, password: _password);
+  //   }
+  //   final channel = IOWebSocketChannel.connect("ws://$serverUrl/api/socket",
+  //       headers: <String, dynamic>{"Cookie": _cookie});
+  //   return channel.stream;
+  // }
 }
