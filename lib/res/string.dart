1de73d1a7473f6300ad20d59dee7b028aa733f54
Souvik Biswas <sbis1999@gmail.com>
2021-01-28 12:27:19 +0530

    Add string consts

---
+++ lib/res/string.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_stream/secrets.dart';
+
+const muxBaseUrl = 'https://api.mux.com';
+const contentType = 'application/json';
+const authToken = '$accessTokenMUX:$secretTokenMUX';
