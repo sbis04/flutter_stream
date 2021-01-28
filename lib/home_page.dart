9c16f9c0f9da5acc0e30e661e648823a1b8ff966
Souvik Biswas <sbis1999@gmail.com>
2021-01-28 19:14:01 +0530

    Home Page update

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_stream/model/video_data.dart';
 import 'package:flutter_stream/utils/mux_client.dart';
+import 'package:video_player/video_player.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,6 +10,19 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   MUXClient _muxClient = MUXClient();
+  VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+        'https://stream.mux.com/rs015pF71Llnoyn02XiN9VdzIF2Zeuj5YkQp2ygYZYsFc.m3u8')
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+    _controller.play();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +44,15 @@ class _HomePageState extends State<HomePage> {
                   'VIDEO ID: ${videoData.data.id}\nStatus: ${videoData.data.status}\nPlaybackIDs: ${videoData.data.playbackIds[0].id}');
             },
           ),
+          Center(
+            child: _controller.value.initialized
+                ? AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: VideoPlayer(_controller),
+                  )
+                : Container(),
+          ),
+
           // StreamBuilder(
           //   stream: _muxClient.channel.stream,
           //   builder: (context, snapshot) {
