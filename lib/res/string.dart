064b18c4e0208f98a5f363b9e51b1c108cf29be3
Souvik Biswas <sbis1999@gmail.com>
2021-01-28 13:47:23 +0530

    Add demo video url and playback policy

--- lib/res/string.dart
+++ lib/res/string.dart
@@ -3,3 +3,6 @@ import 'package:flutter_stream/secrets.dart';
 const muxBaseUrl = 'https://api.mux.com';
 const contentType = 'application/json';
 const authToken = '$accessTokenMUX:$secretTokenMUX';
+const demoVideoUrl =
+    'https://storage.googleapis.com/muxdemofiles/mux-video-intro.mp4';
+const playbackPolicy = 'public';
