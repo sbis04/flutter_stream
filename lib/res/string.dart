472c4b48fa2c539ec0e936308d7173f42f11bf75
Souvik Biswas <sbis1999@gmail.com>
2021-01-29 22:55:05 +0530

    Add constants

--- lib/res/string.dart
+++ lib/res/string.dart
@@ -1,8 +1,13 @@
 import 'package:flutter_stream/secrets.dart';
 
 const muxBaseUrl = 'https://api.mux.com';
+const muxImageBaseUrl = 'https://image.mux.com';
+const muxStreamBaseUrl = 'https://stream.mux.com';
+const videoExtension = 'm3u8';
+const imageTypeSize = 'thumbnail.jpg?time=5&width=200';
 const contentType = 'application/json';
 const authToken = '$accessTokenMUX:$secretTokenMUX';
 const demoVideoUrl =
     'https://storage.googleapis.com/muxdemofiles/mux-video-intro.mp4';
+// https://firebasestorage.googleapis.com/v0/b/sofia-8d800.appspot.com/o/videos%2Fdemo.mp4?alt=media&token=9f64d230-2553-47f2-9c26-e217f8671e30
 const playbackPolicy = 'public';
