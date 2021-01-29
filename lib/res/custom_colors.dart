809b4a9180c672022a42dea9ee85a088e2e2a1c7
Souvik Biswas <sbis1999@gmail.com>
2021-01-29 14:45:06 +0530

    Add few more custom colors

--- lib/res/custom_colors.dart
+++ lib/res/custom_colors.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class CustomColors {
   static const Color muxPink = Color(0xFFff1f78);
   static const Color muxPinkLight = Color(0xFFff709a);
+  static const Color muxPinkVeryLight = Color(0xFFfef6f7);
   static const Color muxOrange = Color(0xFFff4b03);
   static const Color muxOrangeLight = Color(0xFFff7d6e);
+  static const Color muxGray = Color(0xFF383838);
+  static const Color muxGrayLight = Color(0xFFf9f9f9);
 }
